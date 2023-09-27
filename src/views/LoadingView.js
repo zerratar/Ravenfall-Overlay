33bb9e9ac707620cc3827ef7e5eeb08b192319c8
Trippeh <rubbermoose@gmail.com>
2023-09-27 20:27:56 +0200

    First commit, smoll changes: - Removed old resources. - Added new skills. - Added new 'non-formatted' coins value. - Added myself to devs and myself as debug user. - Fixed a comment not being commented out properly.

--- src/views/LoadingView.js
+++ src/views/LoadingView.js
@@ -1,42 +1,41 @@
 import { MainView } from "./BaseViews.js";
 
-
 export class LoadingView extends MainView {
-    constructor() {
-        super('loading');
-    }
+	constructor() {
+		super('loading');
+	}
 
-    onEnter() {
-        console.log("Loading extension...");
-        let twitch = window.Twitch.ext;
-        // update isLocalTest from modules/states.js
-        if (isLocalTest === true) {http://localhost:5500/src/
-            console.warn("We are running in debug mode. This will not work on Twitch!");
-            // note(zerratar): auth token must be set in production
-            Viewer.userId = debug_viewer.id;
-            Ravenfall.service.setAuthInfo({ 
-                channelId: debug_streamer.id, 
-                userId: debug_viewer.id, 
-                token: null,
-                helixToken: null,
-            });
-        } else {
-            if (typeof twitch != 'undefined') {
-                twitch.onContext(function (context) {
-                    // console.log(context);
-                    Ravenfall.service.setContext(context);
-                });
+	onEnter() {
+		console.log("Loading extension...");
+		let twitch = window.Twitch.ext;
+		// update isLocalTest from modules/states.js
+		if (isLocalTest === true) { // http://localhost:5500/src/
+			console.warn("We are running in debug mode. This will not work on Twitch!");
+			// note(zerratar): auth token must be set in production
+			Viewer.userId = debug_viewer.id;
+			Ravenfall.service.setAuthInfo({
+				channelId: debug_streamer.id,
+				userId: debug_viewer.id,
+				token: null,
+				helixToken: null,
+			});
+		} else {
+			if (typeof twitch != 'undefined') {
+				twitch.onContext(function (context) {
+					// console.log(context);
+					Ravenfall.service.setContext(context);
+				});
 
-                twitch.onAuthorized(function (auth) {
-                    if (window.Twitch.ext.viewer.isLinked == false) {
-                        twitch.actions.requestIdShare();
-                    } else {
-                        Ravenfall.service.setAuthInfo(auth);
-                    }
-                });
-            } else {
-                console.error("Twitch extension could not be properly initialized. 'window.Twitch.ext' is undefined.");
-            }
-        }
-    }
+				twitch.onAuthorized(function (auth) {
+					if (window.Twitch.ext.viewer.isLinked == false) {
+						twitch.actions.requestIdShare();
+					} else {
+						Ravenfall.service.setAuthInfo(auth);
+					}
+				});
+			} else {
+				console.error("Twitch extension could not be properly initialized. 'window.Twitch.ext' is undefined.");
+			}
+		}
+	}
 }
